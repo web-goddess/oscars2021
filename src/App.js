9fa79ad78b8c4b59fe60e5c0ed0eed0c03b50ef7
Kris <kris.howard@gmail.com>
2021-04-19 19:58:56 +0200

    Added image of the monkeys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './oscar2021.png';
+import monkeys from './rosefamily.png';
 
 import {Container, Button, Form} from 'react-bootstrap';
 
@@ -69,7 +70,8 @@ function App() {
         <li> Once all the categories are announced, the winner will be the person whose final entry predicted the most categories correctly.</li>
         <li> In the event of a tie, the tie-breaker will be whoever comes closest to the number of celebrities featured in the "In Memoriam" reel.</li>
         <li> If that still doesn't break the tie, the winner will be whoever's entry was submitted first.</li>
-        <li> The winner will receive one custom-made set of four sock monkeys modelled after the Rose family from <em>Schitt's Creek</em>!</li>
+        <li> The winner will receive one custom-made set of four sock monkeys modelled after the Rose family from <em>Schitt's Creek</em>! (Scroll down to the bottom to see them, or head to <a href="https://www.web-goddess.org/archive/22175">my blog</a> for details.)</li>
+        <li> The contest is open for entries until a few hours before the Academy Awards on Sunday, April 25th. I'll announce the winner the next morning when I get up and calculate the scores.</li>
         <li> If the winner's email address is invalid or they don't respond within a week, I reserve the right to send the prize to the next highest scoring entry.</li>
         <li> It's absolutely free to enter, and I'll bear the cost of shipping the monkeys to you. Just pay it forward sometime, okay?</li></ol>
         <Form>
@@ -234,6 +236,9 @@ function App() {
           <Button onClick={addEntry}>Submit your entry</Button>
         </Form>
         &nbsp;
+      </div>
+      <div align="center">
+      <p><img src={monkeys} alt="Rose Family Sock Monkeys" className="monkeys" /></p>
       </div>
     </Container>
   );
